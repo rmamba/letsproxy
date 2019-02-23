d4d42e4ae974c06f86bd4d22f840b16c1cef5270
Red Mamba <rmamba@gmail.com>
2019-02-23 22:46:25 +0000

    css fix

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'letsProxy',
+    script: 'index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    combine_logs: true,
+    max_memory_restart: '256M',
+    env: {
+      PORT: 3000
+    }
+  }]
+};
