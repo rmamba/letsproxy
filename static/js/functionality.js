4fef2de34c142cb94203ee9af9d9111940489bf8
Red Mamba <rmamba@gmail.com>
2019-02-27 17:31:09 +0000

    Fix CSS

--- static/js/functionality.js
+++ static/js/functionality.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
         MicroModal.show('modal-config');
     });
 
+    // $("i[name='extraOptions']").click(function() {
+    //     var domain = this.id;
+    //     // loadConfig(domain);
+    //     MicroModal.show('address-extra');
+    // });
+
     function loadConfig(domain) {
         $('#modal-config-title').html(domain);
         $.ajax({
@@ -18,16 +24,31 @@ $(document).ready(function() {
         });
     }
 
+    upstreamExtraEvents();
     MicroModal.init();
 });
+
+function upstreamExtraEvents() {
+    $("select[name='upstreamExtra[]']:not(.bound)").addClass('bound').click(function() {
+        var port = $(this).data('port');
+        console.log(port);
+        var id = $(this).val();
+        if (id === '1') {
+            $('#weight-'+port).show();
+        } else {
+            $('#weight-'+port).hide();
+        }
+    });
+}
 
 var cnt=1;
 function addUpstreamAddress() {
     if (cnt<7) {
         var wrapper = $('.addresses');
         var port = 3000+Math.floor(Math.random() * 1000);
-        wrapper.append('<div id="port-'+port+'"><input type="text" name="upstreamAddresses[]" placeholder="127.0.0.1">: <input type="text" name="upstreamPorts[]" placeholder="'+port+'"><i class="material-icons pointer delete" onclick="removeUpstreamAddress('+port+');">remove_circle_outline</i></div>');
+        wrapper.append('<div id="port-'+port+'"><input type="text" name="upstreamAddresses[]" placeholder="127.0.0.1">: <input type="text" name="upstreamPorts[]" placeholder="'+port+'" style="width: 65px;"> <select name="upstreamExtra[]" data-port="'+port+'"><option value="0">Default</option><option value="1">Weight</option><option value="2">Backup</option><option value="3">Down</option></select><input id="weight-'+port+'" type="text" name="upstreamWeight[]" data-port="'+port+'" placeholder=">=1" style="width: 50px; display: none;"><i class="material-icons pointer delete medium-icon" onclick="removeUpstreamAddress('+port+');">remove_circle_outline</i></div>');
         cnt++;
+        upstreamExtraEvents();
     }
 }
 
