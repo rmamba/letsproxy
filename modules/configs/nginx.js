68c4d2791cca5d700ff995a02454df5ba06dcab9
Red Mamba <rmamba@gmail.com>
2019-07-08 22:42:26 +0200

    Fix nginx config

--- modules/configs/nginx.js
+++ modules/configs/nginx.js
@@ -146,16 +146,23 @@ module.exports = class Nginx {
             config += `}\n`;
         }
 
-        config += `\nserver {\n`;
-        config += `\tlisten 443 ssl;\n`;
-        config += `\tserver_name ${domain.toLowerCase()};\n`;
-        config += `\taccess_log /var/log/nginx/${domain.toLowerCase()}.access.log;\n`;
-        config += `\terror_log /var/log/nginx/${domain.toLowerCase()}.error.log;\n`;
-
         var isCert = '#';
         if (fs.existsSync(`${CONFIG.acme.certificates}/${domain.toLowerCase()}/fullchain`)) {
             isCert = '';
         }
+
+        config += `\nserver {\n`;
+        config += `\tlisten 443`;
+
+        if (isCert === '') {
+            config += 'ssl';
+        }
+        config += ';\n';
+
+        config += `\tserver_name ${domain.toLowerCase()};\n`;
+        config += `\taccess_log /var/log/nginx/${domain.toLowerCase()}.access.log;\n`;
+        config += `\terror_log /var/log/nginx/${domain.toLowerCase()}.error.log;\n`;
+
         config += `${isCert}\tssl_dhparam ${CONFIG.nginx}/dhparam.pem;\n`;
         config += `${isCert}\tssl_prefer_server_ciphers on;\n`;
         config += `${isCert}\tssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';\n`;
