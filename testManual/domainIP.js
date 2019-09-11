41fcc9ae7344cfbd969033176a7a04ad5d23db61
Red Mamba <rmamba@gmail.com>
2019-09-11 12:14:39 +0700

    Fix ESLint syntax

--- testManual/domainIP.js
+++ testManual/domainIP.js
@@ -1,10 +1,10 @@
-/*jslint es6 node:true */
-"use strict";
+/* eslint  */
+'use strict'
 
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test'
 
-const Dig = require('../modules/dig');
-const dig1 = new Dig('krneki.mambix.com');
-console.log(dig1.ip());
-const dig2 = new Dig('ta.domena.ne.obstaja.com');
-console.log(dig2.ip());
+const Dig = require('../modules/dig')
+const dig1 = new Dig('krneki.mambix.com')
+console.log(dig1.ip())
+const dig2 = new Dig('ta.domena.ne.obstaja.com')
+console.log(dig2.ip())
