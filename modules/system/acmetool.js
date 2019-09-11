1ddb5f06cf68bcd1d51fa0d0ae99b8bb3300f31e
Red Mamba <rmamba@gmail.com>
2019-09-11 12:31:01 +0700

    Fix ESLint formating

--- modules/system/acmetool.js
+++ modules/system/acmetool.js
@@ -1,68 +1,77 @@
-/*jslint es6 node:true */
-//@ts-check
-"use strict";
+/* eslint */
+// @ts-check
+'use strict'
 
-const { exec } = require('child_process');
+const {
+  exec
+} = require('child_process')
 
 module.exports = class Acmetool {
-    constructor() {
-        this.SUDO = 'sudo ';
-        this.BATCH = '';
-    }
+  constructor () {
+    this.SUDO = 'sudo '
+    this.BATCH = ''
+    this.DEBUG = process.env.DEBUG === 'true'
+  }
 
-    want(domains) {
-        const DOMAINS = domains
-        const SUDO = this.SUDO;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}acmetool want ${DOMAINS}`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject(err);
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    resolve(true);
-                }
-            });
-        });
+  log (msg) {
+    if (this.DEBUG) {
+      console.log(msg)
     }
+  }
 
-    status() {
-        const SUDO = this.SUDO;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}acmetool status`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject(err);
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    resolve(true);
-                }
-            });
-        });
-    }
+  want (domains) {
+    const DOMAINS = domains
+    const SUDO = this.SUDO
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}acmetool want ${DOMAINS}`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          resolve(true)
+        }
+      })
+    })
+  }
 
-    run() {
-        const SUDO = this.SUDO;
-        const BATCH = this.BATCH;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}acmetool${BATCH}`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject(err);
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    resolve(true);
-                }
-            });
-        });
-    }
+  status () {
+    const SUDO = this.SUDO
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}acmetool status`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          resolve(true)
+        }
+      })
+    })
+  }
+
+  run () {
+    const SUDO = this.SUDO
+    const BATCH = this.BATCH
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}acmetool${BATCH}`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          resolve(true)
+        }
+      })
+    })
+  }
 }
--- modules/system/nginx.js
+++ modules/system/nginx.js
@@ -1,120 +1,129 @@
-/*jslint es6 node:true */
-//@ts-check
-"use strict";
+/* eslint */
+// @ts-check
+'use strict'
 
-const { exec } = require('child_process');
+const {
+  exec
+} = require('child_process')
 
 module.exports = class Nginx {
-    constructor() {
-        this.SUDO = 'sudo ';
-    }
+  constructor () {
+    this.SUDO = 'sudo '
+    this.DEBUG = process.env.DEBUG === 'true'
+  }
 
-    start() {
-        const SUDO = this.SUDO;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}nginx`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject('ERROR');
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    if (stderr.indexOf('nginx: [emerg]') === -1) {
-                        resolve('OK');
-                    } else {
-                        resolve('ERROR');
-                    }
-                }
-            });
-        });
+  log (msg) {
+    if (this.DEBUG) {
+      console.log(msg)
     }
+  }
 
-    stop() {
-        const SUDO = this.SUDO;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}nginx -s stop`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject('ERROR');
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    if (stderr.indexOf('error') === -1) {
-                        resolve('OK');
-                    } else {
-                        resolve('ERROR');
-                    }
-                }
-            });
-        });
-    }
+  start () {
+    const SUDO = this.SUDO
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}nginx`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          if (stderr.indexOf('nginx: [emerg]') === -1) {
+            resolve('OK')
+          } else {
+            resolve('ERROR')
+          }
+        }
+      })
+    })
+  }
 
-    reload() {
-        const SUDO = this.SUDO;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}nginx -s reload`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject('ERROR');
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    if (stderr === '') {
-                        resolve('OK');
-                    } else {
-                        resolve('ERROR');
-                    }
-                }
-            });
-        });
-    }
+  stop () {
+    const SUDO = this.SUDO
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}nginx -s stop`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          if (stderr.indexOf('error') === -1) {
+            resolve('OK')
+          } else {
+            resolve('ERROR')
+          }
+        }
+      })
+    })
+  }
 
-    test() {
-        const SUDO = this.SUDO;
-        return new Promise(function(resolve, reject){
-            exec(`${SUDO}nginx -t`, (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject('ERROR');
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    if (stderr.indexOf('syntax is ok') > 0) {
-                        resolve('OK');
-                    } else {
-                        resolve('ERROR');
-                    }
-                }
-            });
-        });
-    }
+  reload () {
+    const SUDO = this.SUDO
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}nginx -s reload`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          if (stderr === '') {
+            resolve('OK')
+          } else {
+            resolve('ERROR')
+          }
+        }
+      })
+    })
+  }
 
-    running() {
-        return new Promise(function(resolve, reject){
-            exec('ps waux | grep nginx', (err, stdout, stderr) => {
-                if (err) {
-                  // node couldn't execute the command
-                  console.log(err);
-                  reject('ERROR');
-                } else {
-                    // the *entire* stdout and stderr (buffered)
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
-                    if (stdout.indexOf('nginx: master process') > 0) {
-                        resolve('RUNNING');
-                    } else {
-                        resolve('STOPPED');
-                    }
-                }
-            });
-        });
-    }
+  test () {
+    const SUDO = this.SUDO
+    return new Promise(function (resolve, reject) {
+      exec(`${SUDO}nginx -t`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          if (stderr.indexOf('syntax is ok') > 0) {
+            resolve('OK')
+          } else {
+            resolve('ERROR')
+          }
+        }
+      })
+    })
+  }
+
+  running () {
+    return new Promise(function (resolve, reject) {
+      exec('ps waux | grep nginx', (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          console.error(err)
+          reject(err)
+        } else {
+          // the *entire* stdout and stderr (buffered)
+          this.log(`stdout: ${stdout}`)
+          this.log(`stderr: ${stderr}`)
+          if (stdout.indexOf('nginx: master process') > 0) {
+            resolve('RUNNING')
+          } else {
+            resolve('STOPPED')
+          }
+        }
+      })
+    })
+  }
 }
