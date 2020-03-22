f465a1a8e04e2c2a83618111b00faac842f91e6f
Red Mamba <rmamba@gmail.com>
2020-03-22 21:25:36 +0700

    Fix duplicated configurations

--- modules/configs/nginx.js
+++ modules/configs/nginx.js
@@ -20,7 +20,7 @@ module.exports = class Nginx {
     this.domainsDict = {}
     this.backendsDict = {}
     this.responses = {}
-    this.IGNORE = ['aliases', 'enabled', 'path', 'template', 'location', 'httpRedirect']
+    this.IGNORE = ['aliases', 'enabled', 'path', 'template', 'location', 'httpRedirect', 'ssl_dhparam', 'ssl_prefer_server_ciphers', 'ssl_ciphers']
     if (fs.existsSync(this.FRONTEND_CONFIG)) {
       this.domainsDict = JSON.parse(fs.readFileSync(this.FRONTEND_CONFIG).toString())
     }
@@ -174,6 +174,10 @@ module.exports = class Nginx {
     var ret = true
     this.responses[domain] = undefined
 
+    D.ssl_dhparam = `${CONFIG.nginx}/dhparam.pem`
+    D.ssl_prefer_server_ciphers = 'on'
+    D.ssl_ciphers = '\'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA\''
+
     if (Object.prototype.hasOwnProperty.call(D, 'template')) {
       if (D.template !== '') {
         this.assignTemplate(domain, D.template)
@@ -223,9 +227,15 @@ module.exports = class Nginx {
     config += `\taccess_log /var/log/nginx/${domain.toLowerCase()}.access.log;\n`
     config += `\terror_log /var/log/nginx/${domain.toLowerCase()}.error.log;\n`
 
-    config += `${isCert}\tssl_dhparam ${CONFIG.nginx}/dhparam.pem;\n`
-    config += `${isCert}\tssl_prefer_server_ciphers on;\n`
-    config += `${isCert}\tssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';\n`
+    if (D.ssl_dhparam) {
+      config += `${isCert}\tssl_dhparam ${D.ssl_dhparam};\n`
+    }
+    if (D.ssl_prefer_server_ciphers) {
+      config += `${isCert}\tssl_prefer_server_ciphers ${D.ssl_prefer_server_ciphers};\n`
+    }
+    if (D.ssl_ciphers) {
+      config += `${isCert}\tssl_ciphers ${D.ssl_ciphers};\n`
+    }
     config += `${isCert}\tssl_certificate ${CONFIG.acme.certificates}/${domain.toLowerCase()}/fullchain;\n`
     config += `${isCert}\tssl_certificate_key ${CONFIG.acme.certificates}/${domain.toLowerCase()}/privkey;\n`
 
