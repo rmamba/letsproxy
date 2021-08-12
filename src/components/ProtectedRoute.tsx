63fa565ab03335f86296a6e044e30b34945bf09d
FrostyDe <coc.dwiki@gmail.com>
2021-08-12 08:19:00 +0700

    add protected route for the authorized user

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+
+interface ProtectedRouteProps extends RouteProps {}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ ...rest }) => {
+  const token = localStorage.getItem("token");
+
+  if (token === null) return <Redirect to="/login" />;
+  return <Route {...rest} />;
+};
+
+export default ProtectedRoute;
